28ce9148e36a36dbc1fbc9376f6d17e1f3aad5a1
kipngenohaaron <kipngenohaaron@gmail.com>
2024-11-20 10:13:10 +0300

    homepage  section updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './assets/main.css';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/main.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
