23087aa6f4e290163132a87b7e491385c034c25c
kipngenohaaron <kipngenohaaron@gmail.com>
2024-11-20 10:09:42 +0300

    homepage done

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,3 +1,15 @@
+// import React from 'react';
+
+// const Homepage = () => {
+//   return (
+//     <section className="hero" id="home">
+//       <h1>Welcome to Ujuzi Vijana</h1>
+//       <p>Empowering Youth with Skills for a Brighter Future</p>
+//     </section>
+//   );
+// };
+
+// export default Homepage;
 import React from 'react';
 
 const Homepage = () => {
