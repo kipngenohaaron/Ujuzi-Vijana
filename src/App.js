8a4f27d2ad50fc95de4f6d35df1bf349c37dad07
kipngenohaaron <kipngenohaaron@gmail.com>
2024-11-20 10:08:55 +0300

    header updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
+// import React from 'react';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import Homepage from './components/Homepage';
+// import About from './components/About';
+// import Programs from './components/Programs';
+// import Contact from './components/Contact';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Homepage />
+//       <About />
+//       <Programs />
+//       <Contact />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Homepage from './components/Homepage';
@@ -8,14 +31,16 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <Homepage />
-      <About />
-      <Programs />
-      <Contact />
+      <Switch>
+        <Route path="/" exact component={Homepage} />
+        <Route path="/about" component={About} />
+        <Route path="/programs" component={Programs} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,24 @@
+// import React from 'react';
+
+// const Header = () => {
+//   return (
+//     <header className="header">
+//       <h1>Ujuzi Vijana</h1>
+//       <nav>
+//         <ul className="nav-links">
+//           <li><a href="#home">Home</a></li>
+//           <li><a href="#about">About</a></li>
+//           <li><a href="#programs">Programs</a></li>
+//           <li><a href="#contact">Contact</a></li>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// };
+
+// export default Header;
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -6,10 +26,10 @@ const Header = () => {
       <h1>Ujuzi Vijana</h1>
       <nav>
         <ul className="nav-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#programs">Programs</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/programs">Programs</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
       </nav>
     </header>
