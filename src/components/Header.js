bb9d1f07ad66307b935fc0d370fcdc02803ce6e5
kipngenohaaron <kipngenohaaron@gmail.com>
2024-11-20 09:59:47 +0300

    styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,15 @@
-// src/components/Header.js
 import React from 'react';
-import './Header.css';
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo">Ujuzi Vijana</div>
+      <h1>Ujuzi Vijana</h1>
       <nav>
         <ul className="nav-links">
           <li><a href="#home">Home</a></li>
-          <li><a href="#about">About Us</a></li>
+          <li><a href="#about">About</a></li>
           <li><a href="#programs">Programs</a></li>
-          <li><a href="#contact">Contacts</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
     </header>
